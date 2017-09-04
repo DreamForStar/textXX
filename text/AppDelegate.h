9d3096c8ddeb78803f0498067f704716fe5a4ca2
dreamforstar <dreamforstar@ML.local>
2017-09-04 23:03:16 +0800

    xxxxx

---
+++ text/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text
+//
+//  Created by dreamforstar on 2017/9/4.
+//  Copyright © 2017年 com.jiankeyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text
+//
+//  Created by dreamforstar on 2017/9/4.
+//  Copyright © 2017年 com.jiankeyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
